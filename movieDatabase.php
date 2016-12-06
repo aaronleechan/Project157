fc0a914276341ce0ca3c92adf55cd0cdac023a42
David <dale7642@gmail.com>
2016-12-05 16:04:14 -0800

    git commit

--- movieDatabase.php
+++ movieDatabase.php
@@ -9,16 +9,43 @@
 	</head>
 	<body>
 		<h1>CS157A Project</h1>
-		<B>Team Members:</B> Myat Noe , Eddy, David, Luan<br /><br />
+		<B>Team Members:</B>Myat Noe, Enkhtur Badamsaikhan, David Lerner, Luan Nguyen<br /><br />
 		<hr>
 		<B>Relations:</B>
 		<ol>
 			<li><a href="/Project157/movieDatabase.php/?query=select * from movies;" >Movies</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from actors;" >Actors</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from users;" >Users</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from studios;" >Studios</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from languages;" >Languages</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from directors;" >Directors</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from critics;" >Critics</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from audience;" >Audience</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from professional;" >Professional</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from performs;" >Performs</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from likes;" >Likes</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from reviews;" >Reviews</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from owns;" >Owns</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from has;" >Has</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from directs;" >Directs</a></li>
 		</ol>
 		<hr>
 		<B>Queries:</B>
 		<ol>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from movies where year = 2008;" >Query 1</a>&nbsp;Find all Movies that were released after 2005 and remove duplicates</li>
+			<li><a href="/Project157/movieDatabase.php/?query=SELECT * from movies WHERE year > 2000;" >Query 1</a>&nbsp;Find all Movies that were released after 2000</li>
+			<li><a href="/Project157/movieDatabase.php/?query=SELECT name from movies ORDER BY name;" >Query 2</a>&nbsp;Find the Movie title in ascending order</li>
+			<li><a href="/Project157/movieDatabase.php/?query=SELECT name, languageName FROM movies m 
+			JOIN has h ON m.movieId = h.movieID 
+			WHERE h.languageName = 'ENGLISH';" >Query 3</a>&nbsp;Find which movies are available with English Language?</li>
+			<li><a href="/Project157/movieDatabase.php/?query=SELECT a.name, m.name as 'Movie Title', p.role FROM actors a 
+			JOIN performs p ON p.actorID = a.actorID 
+			JOIN movies m ON m.movieID = p.movieID 
+			WHERE m.genre = 'Horror';" >Query 4</a>&nbsp;Find all actors who performed in horror movies and what their role was</li>
+			<li><a href="/Project157/movieDatabase.php/?query=SELECT c.name, m.name as 'Movie Title', review, rating FROM reviews r 
+			JOIN movies m ON m.movieID = r.movieID 
+			JOIN audience a ON r.criticID = a.criticID 
+			JOIN critics c ON c.criticID = a.criticID 
+			WHERE m.name = '2001: A Space Odyssey';" >Query 5</a>&nbsp;Find all audience reviews and ratings for a specific movie</li>
 		</ol>
 		<hr>
 		<B>Ad-hoc Query:</B>
@@ -71,7 +98,7 @@
 				}
 
 				}else{
-					echo "It failed";
+					echo "Query failed";
 				}
 
 			?>
