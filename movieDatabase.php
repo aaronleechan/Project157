7614aa13752686bf11a597d207afe6d317c83e06
David <dale7642@gmail.com>
2016-12-09 23:20:33 -0800

    re-added general table

--- movieDatabase.php
+++ movieDatabase.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 	<head>
+		<link href="http://localhost/Project157/styles.css" rel="stylesheet">
 		<title>CS157A Project</title>
                 <meta http-equiv="Content-Type" content="text/html 
                     charset=UTF-8" />
@@ -8,44 +9,44 @@
 			page" />
 	</head>
 	<body>
-		<h1>CS157A Project</h1>
+		<h1>S.V. Movie Database</h1>
 		<B>Team Members:</B>Myat Noe, Enkhtur Badamsaikhan, David Lerner, Luan Nguyen<br /><br />
 		<hr>
 		<B>Relations:</B>
 		<ol>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from movies;" >Movies</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from actors;" >Actors</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from users;" >Users</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from studios;" >Studios</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from languages;" >Languages</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from directors;" >Directors</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from critics;" >Critics</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from audience;" >Audience</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from professional;" >Professional</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from performs;" >Performs</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from likes;" >Likes</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from reviews;" >Reviews</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from owns;" >Owns</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from has;" >Has</a></li>
-			<li><a href="/Project157/movieDatabase.php/?query=select * from directs;" >Directs</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from movies;#results" >Movies</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from actors;#results" >Actors</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from users;#results" >Users</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from studios;#results" >Studios</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from languages;#results" >Languages</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from directors;#results" >Directors</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from critics;#results" >Critics</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from audience;#results" >Audience</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from professional;#results" >Professional</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from performs;#results" >Performs</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from likes;#results" >Likes</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from reviews;#results" >Reviews</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from owns;#results" >Owns</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from has;#results" >Has</a></li>
+			<li><a href="/Project157/movieDatabase.php/?query=select * from directs;#results" >Directs</a></li>
 		</ol>
 		<hr>
 		<B>Queries:</B>
 		<ol>
-			<li><a href="/Project157/movieDatabase.php/?query=SELECT * from movies WHERE year > 2000;" >Query 1</a>&nbsp;Find all Movies that were released after 2000</li>
-			<li><a href="/Project157/movieDatabase.php/?query=SELECT name from movies ORDER BY name;" >Query 2</a>&nbsp;Find the Movie title in ascending order</li>
+			<li><a href="/Project157/movieDatabase.php/?query=SELECT * from movies WHERE year > 2000;#results" >Query 1</a>&nbsp;Find all Movies that were released after 2000</li>
+			<li><a href="/Project157/movieDatabase.php/?query=SELECT name from movies ORDER BY name;#results" >Query 2</a>&nbsp;Find the Movie title in ascending order</li>
 			<li><a href="/Project157/movieDatabase.php/?query=SELECT name, languageName FROM movies m 
 			JOIN has h ON m.movieId = h.movieID 
-			WHERE h.languageName = 'ENGLISH';" >Query 3</a>&nbsp;Find which movies are available with English Language?</li>
+			WHERE h.languageName = 'ENGLISH';#results" >Query 3</a>&nbsp;Find which movies are available with English Language</li>
 			<li><a href="/Project157/movieDatabase.php/?query=SELECT a.name, m.name as 'Movie Title', p.role FROM actors a 
 			JOIN performs p ON p.actorID = a.actorID 
 			JOIN movies m ON m.movieID = p.movieID 
-			WHERE m.genre = 'Horror';" >Query 4</a>&nbsp;Find all actors who performed in horror movies and what their role was</li>
+			WHERE m.genre = 'Horror';#results" >Query 4</a>&nbsp;Find all actors who performed in horror movies and what their role was</li>
 			<li><a href="/Project157/movieDatabase.php/?query=SELECT c.name, m.name as 'Movie Title', review, rating FROM reviews r 
 			JOIN movies m ON m.movieID = r.movieID 
 			JOIN audience a ON r.criticID = a.criticID 
 			JOIN critics c ON c.criticID = a.criticID 
-			WHERE m.name = '2001: A Space Odyssey';" >Query 5</a>&nbsp;Find all audience reviews and ratings for a specific movie</li>
+			WHERE m.name = '2001: A Space Odyssey';#results" >Query 5</a>&nbsp;Find all audience reviews and ratings for a specific movie</li>
 		</ol>
 		<hr>
 		<B>Ad-hoc Query:</B>
@@ -57,7 +58,7 @@
                           (We support Select, Insert, <br>Update and Delete queries)
 					</td>
 					<td>
-                          <textarea rows="10" cols="50" name="query"></textarea>
+                          <textarea class="input"rows="10" cols="50" name="query"></textarea>
                     </td>
 				</tr>
 			<tr align = right>
@@ -77,17 +78,8 @@
 		</FORM>
 
 		<div id="results">
-		<hr>
-			<table width= "600" border= "1" cellpadding = "1" cellspacing= "1">
-			  <tr>
-				<th>movieID</th>
-				<th>name</th>
-				<th>description</th>
-				<th>genre</th>
-				<th>year</th>
-				<th>location</th>
-			  </tr>
- 			  <?php
+			<hr>
+ 			<?php
 				$user = 'root';
 				$pass = '';
 				$db   = 'CS157A_Database';
@@ -104,30 +96,45 @@
 				}
 				else
 				{
+					$query = '';
 					print "We have no query";
 				}
 
-				if($result = mysqli_query($link,$query)){
-				while($row = mysqli_fetch_array($result)){
-					echo "<tr>";
-
-					echo "<td>".$row['movieID']."</td>";
-					echo "<td>".$row['name']."</td>";
-					echo "<td>".$row['description']."</td>";
-					echo "<td>".$row['genre']."</td>";
-					echo "<td>".$row['year']."</td>";
-					echo "<td>".$row['location']."</td>";
-					
-					echo "</tr>";
-				// echo '<pre>',print_r($row,true),'</pre>';
+				function sql_to_html_table($result) {
+					// starting table
+					$delim="\n";
+					$htmltable =  "<table>" . $delim ;   
+					$counter = 0 ;
+					// putting in lines
+					while( $row = $result->fetch_assoc()){
+						if ( $counter===0 ) {
+							// table header
+							$htmltable .=   "<tr>"  . $delim;
+							foreach ($row as $key => $value ) {
+								$htmltable .=   "<th>" . $key . "</th>"  . $delim ;
+							}
+						$htmltable .=   "</tr>"  . $delim ; 
+						$counter = 22;
+						} 
+						// table body
+						$htmltable .=   "<tr>"  . $delim ;
+						foreach ($row as $key => $value ) {
+							$htmltable .=   "<td>" . $value . "</td>"  . $delim ;
+						}
+						$htmltable .=   "</tr>"   . $delim; 
+					}
+					// closing table
+					$htmltable .=   "</table>"   . $delim ; 
+					// return
+					return( $htmltable ) ; 
 				}
-
-				}else{
+				
+				if($result = mysqli_query($link,$query)){
+					echo sql_to_html_table($result);
+				} else {
 					echo "Query failed";
 				}
-
-			  ?>
-			</table>
+			?>
 		</div>
 	</body>
 </html>
